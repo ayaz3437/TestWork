dfb165f0aed38d55f6dbef4d8d52e7f81d35ab13
Turadg Aleahmad <turadg@agoric.com>
2022-10-21 10:59:00 -0700

    feat: link inter.trade

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,14 @@ const App = () => {
       ></ToastContainer>
       <motion.div>
         <motion.div className="min-w-screen container p-4 mx-auto flex justify-between items-center">
-          <img src={INTER_LOGO} className="item" alt="Inter Logo" width="200" />
+          <a href="https://inter.trade/" target="inter.trade">
+            <img
+              src={INTER_LOGO}
+              className="item"
+              alt="Inter Logo"
+              width="200"
+            />
+          </a>
           <WalletConnection />
         </motion.div>
         <motion.div className="min-w-screen container mx-auto flex justify-center mt-16">
