5400b2e263a86adc0ed9f2b3c2401574bedcb8f1
Turadg Aleahmad <turadg@agoric.com>
2022-10-26 11:32:57 -0700

    feat: docs link for each brand

--- src/components/AssetListItem.tsx
+++ src/components/AssetListItem.tsx
@@ -1,31 +1,41 @@
-import { useId } from 'react';
 import { useAtomValue } from 'jotai';
 
-import { displayFunctionsAtom } from 'store/app';
 import { Brand } from '@agoric/ertp';
+import { displayFunctionsAtom } from 'store/app';
+import { FiHelpCircle } from 'react-icons/fi';
+
+const docsFor = (petname: string) =>
+  `https://docs.inter.trade/tokens/${petname}`;
 
 const AssetListItem = ({ brand }: { brand: Brand }) => {
   const { displayBrandIcon, displayBrandPetname } =
     useAtomValue(displayFunctionsAtom);
 
+  const icon = displayBrandIcon(brand);
+  const petname = displayBrandPetname(brand);
+  const docs = docsFor(petname);
+
   return (
     <div
-      key={useId()}
+      key={petname}
       className="flex gap-3 items-center justify-between w-full"
     >
       <div className="flex gap-3 items-center">
         <div className="w-8 h-8 rounded-full">
-          <img
-            className="w-8 h-8"
-            src={displayBrandIcon(brand)}
-            alt={displayBrandPetname(brand)}
-          />
+          <img className="w-8 h-8" src={icon} alt={petname} />
         </div>
 
         <div className="flex flex-col">
-          <h3 className="font-semibold">{displayBrandPetname(brand)}</h3>
+          <h3 className="font-semibold">{petname}</h3>
         </div>
       </div>
+      <a
+        href={docs}
+        target={petname}
+        className="text-xl text-blue-500 hover:text-blue-700"
+      >
+        <FiHelpCircle />
+      </a>
     </div>
   );
 };
